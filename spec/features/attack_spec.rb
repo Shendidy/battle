97f07a8cf5d7553b9ad6e0c58d081e85cd042793
Sherif Shendidy <shendidy@Sherifs-MacBook-Pro.local>
2019-01-27 07:00:46 +0000

    Implementing loss feature and starting a new game

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,22 @@ feature 'Attack' do
     click_button 'Attack'
     expect(page).to have_content 'Mittens: 50'
   end
+
+  scenario 'player 2 looses' do
+    sign_in_and_play
+    11.times { click_button 'Attack' }
+    expect(page).to have_content 'Mittens: 0'
+  end
+
+  scenario 'player 2 looses' do
+    sign_in_and_play
+    11.times { click_button 'Attack' }
+    expect(page).to have_content 'Mittens lost the game. Start a new one?'
+  end
+
+  scenario 'player 2 looses' do
+    sign_in_and_play
+    11.times { click_button 'Attack' }
+    expect(page).to have_button 'New game?'
+  end
 end
